0fe7c16c776f177d4ff1de362099ad7333004226
Bartosz Krynski <krynskibartosz08@gmail.com>
2022-01-27 13:24:25 +0100

    a
    Signed-off-by: Bartosz Krynski <krynskibartosz08@gmail.com>

--- pages/api/mail.ts
+++
@@ -1,7 +0,0 @@
-const mail = (req: any, res: any) => {
-  const body = JSON.parse(req.body);
-  console.log("🚀 ~ file: mail.ts ~ line 3 ~ body", body);
-  res.status(200).json({ status: "OK" });
-};
-
-export default mail;
